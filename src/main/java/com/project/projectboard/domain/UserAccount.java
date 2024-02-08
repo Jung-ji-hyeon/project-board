3f280f3bd77376ab8b2e72b4b810d6e24733cfdb
Jung-ji-hyeon <ejxldortm@naver.com>
2024-02-08 19:41:53 +0900

    #68 - 회원 엔티티 디자인 수정: 인증이 없는 상태에서 회원 정보를 DB에 등록하기 위한 생성자 메소드 추가
    인증 정보가 없을 땐 직접 생성자, 수정자 정보를 입력하여
    회원 등록을 할 수 있도록 생성자 메소드를 추가함

--- src/main/java/com/project/projectboard/domain/UserAccount.java
+++ src/main/java/com/project/projectboard/domain/UserAccount.java
@@ -8,7 +8,7 @@ import lombok.ToString;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
@@ -20,7 +20,6 @@ public class UserAccount extends AuditingFields {
     @Column(length = 50)
     private String userId;
 
-
     @Setter @Column(nullable = false) private String userPassword;
 
     @Setter @Column(length = 100) private String email;
@@ -30,16 +29,22 @@ public class UserAccount extends AuditingFields {
 
     protected UserAccount() {}
 
-    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;
     }
 
     public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+        return new UserAccount(userId, userPassword, email, nickname, memo, null);
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
     }
 
     @Override
@@ -53,8 +58,5 @@ public class UserAccount extends AuditingFields {
     public int hashCode() {
         return Objects.hash(this.getUserId());
     }
-
-
-
 
 }
