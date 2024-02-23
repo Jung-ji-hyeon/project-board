fc2122d8958d459bb2f62286740e27480d1e6cac
Jung-ji-hyeon <ejxldortm@naver.com>
2024-02-23 17:44:55 +0900

    #107 - UserAccount api 노출 추가 + data rest projection 을 이용한 데이터 노출 필드 설정
    `UserAccount`를 노출할 api로 추가해 줌.
    이렇게 하면 다른 게시글, 댓글 등의 data rest api 노출 데이터 형식이
    원하지 않는 스타일로 바뀌게 된다.
    이를 수동으로 설정하기 위해 projection 기능을 사용하여
    노출할 필드들을 직접 지정함.

--- src/main/java/com/project/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/project/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.project.projectboard.repository;
 
 import com.project.projectboard.domain.Article;
 import com.project.projectboard.domain.QArticle;
+import com.project.projectboard.domain.projection.ArticleProjection;
 import com.project.projectboard.repository.querydsl.ArticleRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
@@ -13,7 +14,7 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = ArticleProjection.class)
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         ArticleRepositoryCustom,
